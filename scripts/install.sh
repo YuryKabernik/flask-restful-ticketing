a406e6b514daa8a5d256c19fe48c8a6a6fb02c7f
Yura <yury.kabernik.berazouski@gmail.com>
2024-06-27 07:46:40 +0000

    add(venv) install script

---
+++ scripts/install.sh
@@ -0,0 +1,15 @@
+#!/bin/sh -e
+
+# Use the Python executable provided from the `-p` option, or a default.
+[ "$1" = "-p" ] && PYTHON=$2 || PYTHON="python3"
+
+REQUIREMENTS="requirements.txt"
+VENV="venv"
+
+set -x
+
+"$PYTHON" -m venv "$VENV"
+PIP="$VENV/bin/pip"
+
+"$PIP" install -U pip
+"$PIP" install -r "$REQUIREMENTS"
